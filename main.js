91dcba1d1540feb31d562be6938df7c7c33415b2
Łukasz Tyczka <lukasz@tyczka.net>
2020-12-13 22:56:01 +0100

    Fun with js

--- main.js
+++ main.js
@@ -7,3 +7,29 @@ console.log(
 let content = document.querySelector('.welcome-section__content--js');
 
 content.innerHTML = `Cześć witaj na mojej stronie nazywam się ${firstName} i mam ${age} lata!`;
+
+function Showlog() {
+    console.log('showLog');
+}
+
+Showlog();
+
+function showLog(name, age) {
+    console.log(`czolem ${name}, mam ${age}`);
+}
+
+showLog('Maciek', 4);
+
+const ExampleClass = {
+    name: 'Maciek z classy:D',
+    age: 27,
+    showlog: (name, age) => {
+        console.log(`${name} ma ${age} lat:) `);
+    },
+    showlog2: function () {
+        console.log(`${this.name} SUPER DZIALA ${this.age}`);
+    },
+};
+
+ExampleClass.showlog(ExampleClass.name, ExampleClass.age);
+ExampleClass.showlog2(ExampleClass.name, ExampleClass.age);
